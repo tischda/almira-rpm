8ad351ef65579158784b2e4059135f7397d1126f
tischda <dos.7182@gmail.com>
2013-03-27 14:57:52 +0100

    giving files back to jira

--- packages/atlassian-jira/src/main/scripts/04.postinstall.sh
+++ packages/atlassian-jira/src/main/scripts/04.postinstall.sh
@@ -1,9 +1,9 @@
 # Initial installation
 if [ "$1" = "1" ]; then
   mkdir -p -m775 @{destBase}/{logs,temp,work}
-  chown :@{appGroupName} @{destBase}/{logs,temp,work}
+  chown @{appUserName}:@{appGroupName} @{destBase}/{logs,temp,work}
   mkdir -p -m775 @{appWorkFolder}
-  chown :@{appGroupName} @{appWorkFolder}
+  chown @{appUserName}:@{appGroupName} @{appWorkFolder}
 
   cd /etc/rc.d/init.d
   ln -sf tomcat @{appServiceName}
