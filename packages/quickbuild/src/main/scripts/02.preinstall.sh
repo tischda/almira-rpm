e99e257587130e71d5b00e28d8094fc9e32e4d50
Daniel TISCHER <dos.7182@gmail.com>
2017-02-11 19:24:10 +0100

    make scripts executable again

--- packages/quickbuild/src/main/scripts/02.preinstall.sh
+++ packages/quickbuild/src/main/scripts/02.preinstall.sh
@@ -2,21 +2,7 @@
 getent group @{appGroupName} > /dev/null || groupadd -r @{appGroupName}
 getent passwd @{appUserName} > /dev/null || useradd -r -m -g @{appGroupName} @{appUserName}
 
-# Initial installation => install service
-if [ "$1" = "1" ]; then
-  mv @{destBase}.new @{destBase}
-fi
-
 # Uninstall or Update => stop service
 if [ "$1" = "0" -o "$1" = "2" ]; then
   service @{appServiceName} stop
 fi
-
-# Update => Save old folder for migration, and get rid of symlinks
-#if [ "$1" = "2" ]; then
-#  echo Backing up old version for migration
-#  rm -f @{destBase}/conf
-#  su - @{appUserName} -c "cp -r @{destConf} @{destBase}/conf"
-#  rm -rf @{destBase}.old
-#  mv @{destBase} @{destBase}.old
-#fi
--- packages/quickbuild/src/main/scripts/04.postinstall.sh
+++ packages/quickbuild/src/main/scripts/04.postinstall.sh
@@ -1,19 +1,22 @@
 # Make scripts executable
-chmod -R 755 @{destBase}/bin/*.sh
-
-# Configure startup script
-sed -i 's|chkconfig: 2345 20 80|chkconfig: 2345 70 20|g' @{destBase}/bin/server.sh
-sed -i 's|PIDDIR="."|PIDDIR="/var/run/@{appServiceName}"|g' @{destBase}/bin/server.sh
-sed -i 's|#RUN_AS_USER=|RUN_AS_USER=@{appUserName}|g' @{destBase}/bin/server.sh
+chmod -R 755 @{destBase}.new/bin/*.sh
 
 # Initial installation => install service
 if [ "$1" = "1" ]; then
-  @{destBase}/bin/server.sh install
-fi
+  mv @{destBase}.new @{destBase}
 
-# Link back from /etc
-rm -rf @{destBase}/conf
-ln -sf @{destConf} @{destBase}/conf
+  # Configure startup script
+  sed -i 's|chkconfig: 2345 20 80|chkconfig: 2345 70 20|g' @{destBase}/bin/server.sh
+  sed -i 's|PIDDIR="."|PIDDIR="/var/run/@{appServiceName}"|g' @{destBase}/bin/server.sh
+  sed -i 's|#RUN_AS_USER=|RUN_AS_USER=@{appUserName}|g' @{destBase}/bin/server.sh
+
+  # Perform installation
+  @{destBase}/bin/server.sh install
+
+  # Link back from /etc
+  rm -rf @{destBase}/conf
+  ln -sf @{destConf} @{destBase}/conf
+fi
 
 # Upgrade
 if [ "$1" = "2" ]; then
@@ -21,6 +24,6 @@ if [ "$1" = "2" ]; then
   su - @{appUserName} -c "@{destBase}.new/bin/upgrade.sh @{destBase} && rm -rf @{destBase}.new"
 fi
 
-# Install MySQL connector (do this after the migration which copies old libs)
-rm -f @{destBase}/plugins/com.pmease.quickbuild.libs/mysql-connector-java-*.jar
+# Install MySQL connector (do this after the uprade which copies old libs)
+rm -f @{destBase}/plugins/com.pmease.quickbuild.libs/mysql-connector-java-*.jar 2>/dev/null
 mv @{destBase}/mysql-connector-java-*.jar @{destBase}/plugins/com.pmease.quickbuild.libs
