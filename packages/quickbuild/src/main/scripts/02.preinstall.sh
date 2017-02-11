4d61183c7ca243e46d7b5ae45386f42cca822572
Daniel TISCHER <dos.7182@gmail.com>
2017-02-11 21:49:38 +0100

    fixing again not ok

--- packages/quickbuild/src/main/scripts/02.preinstall.sh
+++ packages/quickbuild/src/main/scripts/02.preinstall.sh
@@ -8,7 +8,14 @@ if [ "$1" = "0" -o "$1" = "2" ]; then
 fi
 
 # Update => Save old folder
+# Removing symlinks because update.sh will copy its files to this location
+# But because it iselfs has them linked to /etc/quickbuild, this will not work
 if [ "$1" = "2" ]; then
+  echo Making a copy of the configuration
+  rm -f @{destBase}/conf
+  su - @{appUserName} -c "cp -r @{destConf} @{destBase}/conf"
+
   echo Backing up old version for upgrade
+  rm -rf @{destBase}.old
   mv @{destBase} @{destBase}.old
 fi
--- packages/quickbuild/src/main/scripts/04.postinstall.sh
+++ packages/quickbuild/src/main/scripts/04.postinstall.sh
@@ -1,25 +1,25 @@
 # Make scripts executable
 chmod -R 755 @{destBase}/bin/*.sh
 
+# Configure startup script
+sed -i 's|chkconfig: 2345 20 80|chkconfig: 2345 70 20|g' @{destBase}/bin/server.sh
+sed -i 's|PIDDIR="."|PIDDIR="/var/run/@{appServiceName}"|g' @{destBase}/bin/server.sh
+sed -i 's|#RUN_AS_USER=|RUN_AS_USER=@{appUserName}|g' @{destBase}/bin/server.sh
+
 # Install MySQL connector (do this before installation or upgrade)
 mv @{destBase}/mysql-connector-java-*.jar @{destBase}/plugins/com.pmease.quickbuild.libs
 
 # Initial installation => install service
 if [ "$1" = "1" ]; then
-  # Configure startup script
-  sed -i 's|chkconfig: 2345 20 80|chkconfig: 2345 70 20|g' @{destBase}/bin/server.sh
-  sed -i 's|PIDDIR="."|PIDDIR="/var/run/@{appServiceName}"|g' @{destBase}/bin/server.sh
-  sed -i 's|#RUN_AS_USER=|RUN_AS_USER=@{appUserName}|g' @{destBase}/bin/server.sh
-
   # Perform installation (as root ?)
   @{destBase}/bin/server.sh install
-
-  # Link back from /etc
-  rm -rf @{destBase}/conf
-  ln -sf @{destConf} @{destBase}/conf
 fi
 
-# Upgrade
+# Link back from /etc
+rm -rf @{destBase}/conf
+ln -sf @{destConf} @{destBase}/conf
+
+# Upgrade and remove temporary installation files
 if [ "$1" = "2" ]; then
 
   # Changing the @{destBase} in the RPM would mess up everything
